6fef1503557fb31e4a411c813a3a27599c62ef52
Shirsha Sengupta <devofshirsha@gmail.com>
2024-05-30 23:02:17 +0530

    [test]: Test some APIs

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const { PORT } = require("./config/serverConfig");
 const dbConnect = require("./config/database");
 
@@ -9,8 +10,8 @@ const setupAndStartServer = async()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.listen(PORT, ()=>{
-        console.log(`Server Running on PORT ${PORT}`);
+    app.listen(8008, ()=>{
+        console.log(`Server Running on PORT`);
         // dbConnect();
     });
 }
---
+++ server/src/services/aadhaarService.js
@@ -0,0 +1,9 @@
+const { AadhaarCardRepository } = require('../repository/index');
+
+class AadhaarCardService{
+    constructor(){
+        this.aadhaarCardRepository = new AadhaarCardRepository();
+    }
+
+    
+}
