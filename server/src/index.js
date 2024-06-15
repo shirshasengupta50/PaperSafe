2ba76619eb5ea5c9da6cedde291017e7faf10408
Shirsha Sengupta <devofshirsha@gmail.com>
2024-06-15 20:59:32 +0530

    [fix] : Add cors for accepting Cross Origin Requests

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { PORT } = require("./config/serverConfig");
 const dbConnect = require("./config/database");
@@ -11,6 +12,7 @@ const app = express();
 const setupAndStartServer = async()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(cors());
 
     app.use('/api', apiRoute);
 
